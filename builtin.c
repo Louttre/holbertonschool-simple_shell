dfda3d7ae0c9483b443543087b7ee637423c2263
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-04-19 01:24:08 +0200

    Create builtin.c

---
+++ builtin.c
@@ -0,0 +1,29 @@
+void check_builtin(char **arg)
+{
+	bl_t array = {
+		{"exit", exit_func},
+		{"env", printenv_func},
+		{NULL, NULL}
+	};
+	int i, j;
+
+	for(i = 0; array[i]; i++)
+	{
+		if (strcmp(array[i].command, arg) == 0)
+			return (array[i].func(arg));
+	}
+}
+void exit_func(args)
+{
+	free_tab(arg);
+	exit(2);
+}
+void printenv_func(char *arg)
+{
+	char **cp_env = environ;
+
+	for (; *cp_env; env++)
+	{
+		printf("%s\n", *env);
+	}
+}
