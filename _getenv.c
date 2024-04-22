977f6adaab6ad7666945e75b6d8bac3dfccd3b2a
root <seth.rueda@gmail.com>
2024-04-22 08:22:19 +0000

    i did smth

--- _getenv.c
+++ _getenv.c
@@ -2,31 +2,31 @@
 #include <string.h>
 char *_getenv(char *env_variable)
 {
-        char *temp;
-        char **cp_env = environ;
-        int i = 0, j;
+	char *temp;
+	char **cp_env = environ;
+	int i = 0, j;
 
-        while (cp_env)
-        {
-                j = 0;
+	while (cp_env)
+	{
+		j = 0;
 		temp = malloc(sizeof(char) * BUF);
-                while (cp_env[i][j] != '=')
-                {
-                        temp[j] = cp_env[i][j];
-                        j++;
-                }
+		while (cp_env[i][j] != '=')
+		{
+			temp[j] = cp_env[i][j];
+			j++;
+		}
 		temp[j] = '\0';
-                if (strcmp(temp, env_variable) == 0)
-                {
-                        free(temp);
-                        return (cp_env[i]);
-                }
+		if (strcmp(temp, env_variable) == 0)
+		{
+			free(temp);
+			return (cp_env[i]);
+		}
                 else
-                {
-                        free(temp);
-                        temp = NULL;
-                }
-                i++;
-        }
-        return (NULL);
+		{
+			free(temp);
+			temp = NULL;
+		}
+		i++;
+	}
+	return (NULL);
 }
