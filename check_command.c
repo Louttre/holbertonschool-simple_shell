83b24505835c33c5b7bcfd88fcc7462229b04d13
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-04-26 18:50:15 +0200

    Update check_command.c

--- check_command.c
+++ check_command.c
@@ -1,5 +1,45 @@
 #include "shell.h"
 
+int path_finding(char *command)
+{
+        struct stat file_stat;
+
+        if (stat(command, &file_stat) != 0)
+        {
+                return (-1);
+        }
+        if (S_ISREG(file_stat.st_mode))
+        {
+                return (access(command, X_OK) == 0 ? 1 : -1);
+        }
+        else if (S_ISDIR(file_stat.st_mode))
+        {
+                return (-1);
+        }
+        else
+        {
+                return (-1);
+        }
+}
+/**
+ * command_is_path - check if the command uses an explicit path syntax
+ * (/usr/bin/ls for example)
+ *
+ * @command: command to be checked
+ *
+ * Return: 1 if command uses an explicit path syntax,
+ * -1 if the command is a wrong PATH, 0 otherwise.
+ */
+int command_is_path(char *command)
+{
+        if (command[0] == '/' || command[0] == '.')
+        {
+                return (path_finding(command));
+        }
+        else
+                return (0);
+}
+
 /**
  * command_is_path - check if the command uses an explicit path syntax
  * (/usr/bin/ls for example)
@@ -28,24 +68,21 @@ int command_is_path(char *command)
  *
  * @command: command to be checked inside the PATH
  * @argv: array of arguments sent by main()
- * @args: input that will be checked to see if it's a builtin command
- * @temp: buffer storing the input to be checked and possibly executed
  *
  * Return: if the path of the command is checked, returns said path,
  * returns NULL otherwise
  */
-char *check_paths(char *command, char **argv, char **args, char *temp)
+char *check_paths(char *command, char **argv)
 {
 	char *path, *cp_path, **paths = NULL;
 	char *check_path = NULL;
 	int i = 0;
 
-	path = _getenv("PATH1");
+	path = _getenv("PATH");
 	if (!path)
 	{
 		fprintf(stderr, "%s: 127: %s: %s\n", argv[0], command, "not found");
-		clean(temp, args);
-		exit(127);
+		return (NULL);
 	}
 	cp_path = strdup(path);
 	if (!cp_path)
@@ -87,14 +124,12 @@ char *check_paths(char *command, char **argv, char **args, char *temp)
  *
  * @command: command to be checked
  * @argv: array containing the arguments sent by main()
- * @args: input that will be checked to see if it's a builtin command
- * @temp: buffer storing the input to be checked and possibly executed
  *
  * Return: command if the command is a valid explicit path, right_path if
  * the command has a valid executable found inside the PATH variable,
  * NULL otherwise
  */
-char *check_command(char *command, char **argv, char **args, char *temp)
+char *check_command(char *command, char **argv)
 {
 	char *right_path;
 
@@ -108,7 +143,7 @@ char *check_command(char *command, char **argv, char **args, char *temp)
 		free(command);
 		return (NULL);
 	}
-	right_path = check_paths(command, argv, args, temp);
+	right_path = check_paths(command, argv);
 	if (right_path)
 		return (right_path);
 	return (NULL);
